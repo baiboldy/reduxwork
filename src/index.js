007300e5bc62f89c28027f468dae1375758eca89
John Wick <baiboldy.dias@gmail.com>
2020-03-05 00:23:07 +0600

    second lesson

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
-function playList(state = [], action) {
+const initialState = [
+    'Smeels like spilit',
+    'Enter smake for'
+]
+
+function playList(state = initialState, action) {
 
     if (action.type === 'ADD_TRACK') {
         return [
@@ -27,23 +24,40 @@ function playList(state = [], action) {
 }
 
 const store = createStore(playList)
-const tranInput = document.querySelectorAll('.trackInput')[0]
-const list = document.querySelectorAll('.list')[0]
-const addTrackButton = document.querySelectorAll('.addTrack')[0]
 
-console.log(store.getState())
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 
-store.subscribe(() => {
-    list.innerHTML = ''
-    tranInput.value = ''
-    store.getState().forEach(element => {
-        const li = document.createElement('li')
-        li.textContent = element
-        list.appendChild(li)
-    });
-})
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
-addTrackButton.addEventListener('click', () => {
-    const trackName = tranInput.value
-    store.dispatch({ type: 'ADD_TRACK', payload: trackName })
-})
+
+// import { createStore } from 'redux'
+
+
+
+// const tranInput = document.querySelectorAll('.trackInput')[0]
+// const list = document.querySelectorAll('.list')[0]
+// const addTrackButton = document.querySelectorAll('.addTrack')[0]
+
+// console.log(store.getState())
+
+// store.subscribe(() => {
+//     list.innerHTML = ''
+//     tranInput.value = ''
+//     store.getState().forEach(element => {
+//         const li = document.createElement('li')
+//         li.textContent = element
+//         list.appendChild(li)
+//     });
+// })
+
+// addTrackButton.addEventListener('click', () => {
+//     const trackName = tranInput.value
+//     store.dispatch({ type: 'ADD_TRACK', payload: trackName })
+// })
